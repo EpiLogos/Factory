1266761186a29ad3aecbedf3da8749c54e0d0e9f
Epii <frank.g.taylor97@gmail.com>
2026-09-10 21:02:17 +0100

    feat(factory): route public attempt subcommand

--- factory/src/bin/factory.rs
+++ factory/src/bin/factory.rs
@@ -1,3 +1,7 @@
 fn main() -> std::process::ExitCode {
+    let args = std::env::args().skip(1).collect::<Vec<_>>();
+    if args.first().map(String::as_str) == Some("attempt") {
+        return epilogos_factory::attempt_runtime::attempt_cli_main(&args[1..]);
+    }
     epilogos_factory::development_field_cli::cli_main()
 }
